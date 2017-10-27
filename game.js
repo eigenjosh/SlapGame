31e4b76d2cc844a99d38f7df2e9519b8e290cde7
Josh Eigen <josh.eigen@gmail.com>
2017-10-27 13:10:57 -0700

    it works oh my god

--- game.js
+++ game.js
@@ -1,33 +1,42 @@
 
-var Target = function(name, health, hits){
+var Target = function (name, health, hits) {
     this.name = name;
     this.health = health;
     this.hits = hits
+    this.items = []
 }
 
-var Item = function(name, physDmg, description){
-	this.name = name;
-	this.physDmg = physDmg;
-	this.description = description
+var Item = function (name, physDmg, description) {
+    this.name = name;
+    this.physDmg = physDmg;
+    this.description = description
 }
+
 
 var targets = new Target("Trusty patches", 100, 0)
 
-var items = new Item("Zweihander",41,"git gud casul")
+var items = new Item("Zweihander", 41, "git gud casul")
 
-function slap(){
+function giveZwei(items) {
+    targets.items.push(items)
+}
+
+giveZwei(items)
+console.log(targets.items)
+
+function slap() {
     targets.health -= 1
     targets.hits += 1
     update()
 }
 
-function punch(){
+function punch() {
     targets.health -= 5
     targets.hits += 1
     update()
 }
 
-function kick(){
+function kick() {
     targets.health -= 10
     targets.hits += 1
     update()
@@ -35,7 +44,7 @@ function kick(){
 
 
 
-function update(){
+function update() {
     document.getElementById('health').innerText = targets.health
     document.getElementById('hits').innerText = targets.hits
     document.getElementById('name').innerText = targets.name
