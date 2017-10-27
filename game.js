71163faf3ada11d0f39435975ab344b6637ffdce
Josh Eigen <josh.eigen@gmail.com>
2017-10-27 12:44:39 -0700

    barely functioning refactored code

--- game.js
+++ game.js
@@ -1,52 +1,36 @@
-var health = 100
-var name = "Trusty Patches"
-var hits = 0
 
-var targets = {
-    
-    trustyPatches: new Targets('Trusty Patches', 100, 0, 0)
-}
-
-
-function Targets(name, health, hits, physicalResistance){
+var Targets = function(name, health, hits){
     this.name = name
     this.health = health
     this.hits = hits
-    this.physicalResistance = physicalResistance
-
-
 }
 
-var Items = function(physicalDmg,  option3){
-	this.option1 = option1;
-	this.option2 = option2;
-	this.option3 = option3;
-}
+var patches = new Targets("Trusty Patches", 100, 0)
 
 function slap(){
-    health -= 1
-    hits += 1
+    patches.health -= 1
+    patches.hits += 1
     update()
 }
 
 function punch(){
-    health -= 5
-    hits += 1
+    patches.health -= 5
+    patches.hits += 1
     update()
 }
 
 function kick(){
-    health -= 10
-    hits += 1
+    patches.health -= 10
+    patches.hits += 1
     update()
 }
 
 
 
 function update(){
-    document.getElementById('health').innerText = health
-    document.getElementById('hits').innerText = hits
-    document.getElementById('name').innerText = name
+    document.getElementById('health').innerText = patches.health
+    document.getElementById('hits').innerText = patches.hits
+    document.getElementById('name').innerText = patches.name
 }
 
 update()
