f1d7f327ec51890ade7cb5f283600cb5c8afbb58
Josh Eigen <josh.eigen@gmail.com>
2017-10-27 12:11:02 -0700

    code strategically broken

--- game.js
+++ game.js
@@ -1,6 +1,24 @@
-var health = 100
-var name = "Trusty Patches"
-var hits = 0
+
+
+var targets = {
+    
+    trustyPatches: new Targets('Trusty Patches', 100, 0, 0)
+}
+
+
+function Targets(name, health, hits, physicalResistance){
+    this.name = name
+    this.health = health
+    this.hits = hits
+    this.physicalResistance = physicalResistance
+
+
+}
+
+var Items = function(physicalDmg,  option3){
+	this.option1 = option1;
+	this.option2 = option2;
+	this.option3 = option3;
 
 
 function slap(){
@@ -24,9 +42,9 @@ function kick(){
 
 
 function update(){
-    document.getElementById('health').innerText = health
-    document.getElementById('hits').innerText = hits
-    document.getElementById('name').innerText = name
+    document.getElementById('health').innerText = targets.health
+    document.getElementById('hits').innerText = targets.hits
+    document.getElementById('name').innerText = targets.name
 }
 
 update()
